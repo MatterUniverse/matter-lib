b83eb910696ed39c13a24a3c8edbe7447f8647eb
Attila <matterapp@protonmail.com>
2019-03-25 20:44:32 -0700

    build good

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var ts = require("gulp-typescript");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
+let uglify = require('gulp-uglify-es').default;
+// var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
 var paths = {
     pages: ['src/examples/*.html']
 };
@@ -23,5 +27,21 @@ gulp.task("build", ['copy-html'], function () {
   .plugin(tsify)
   .bundle()
   .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(uglify())
   .pipe(gulp.dest("dist"));
+/*return browserify({
+      basedir: '.',
+      debug: true,
+      entries: ['src/index.ts'],
+      cache: {},
+      packageCache: {}
+  })
+  .plugin(tsify)
+  .bundle()
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(source('bundle.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest("dist"));*/
 });
